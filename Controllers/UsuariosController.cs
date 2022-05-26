fdbfc84bc6e9e2714cde1431dd96ff9539ccf103
cristian <144996@unsaac.edu.pe>
2022-05-26 18:57:45 -0500

    First Commit

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Notaria.Context;
+using Notaria.Models;
+
+namespace Notaria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Usuarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuarios>>> Getusuarios()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+
+
+
+        [HttpGet("{username}/{password}")]
+        public ActionResult<List<Usuarios>> GetIniciarSesion(string username, string password)
+        {
+            var usuarios = _context.Usuarios.Where(usuario => usuario.USUARIO.Equals(username) && usuario.CLAVE.Equals(password)).ToList();
+
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+            Console.WriteLine("si entró");
+            return usuarios;
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Notaria.Context;
+
+namespace Notaria
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup (IConfiguration iconfiguration)
+        {
+            Configuration = iconfiguration;
+        }
+
+        public IConfiguration Configuration { get;}
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Enable CORS
+            services.AddCors();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Conexion")));
+            //JSON SERIALIZER
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver =
+                new DefaultContractResolver());
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+           
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseCors(options =>
+            {
+                options.WithOrigins("http://localhost:3000");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("index.html");
+            app.UseDefaultFiles(options);
+            app.UseStaticFiles();
+
+
+
+
+        }
+    }
+}
